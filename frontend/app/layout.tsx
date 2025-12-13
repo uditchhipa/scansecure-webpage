722576510eea50489b1067083a1a39b8ff2f709a
uditchhipa <uditchhipa27@gmail.com>
2025-12-14 03:18:32 +0530

    Fix: Move AdSense to Head & Use afterInteractive for Verification

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -43,19 +43,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-
-        {/* Google AdSense */}
+      <head>
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2438557878561786"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
-
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
         <Analytics />
       </body>
     </html>
